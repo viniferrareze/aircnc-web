d2004f674909244d79139888ab799169a1869339
carlos vinícius ferrareze <viniferrareze@gmail.com>
2019-10-03 00:29:27 -0300

    iniciado tela de login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
+import api from './services/api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [email, setEmail] = useState('');
+
+	function acessar(event){
+		event.preventDefault(); //ignora evento padrão
+	}
+
+	return (
+		<div className="container">
+			<img src={logo} alt="Aircnc" />
+
+			<div className="content">
+				<p>
+					Ofereça <strong>spots</strong> para programadores e econtre talentos para sua empresa
+				</p>
+
+				<form onSubmit={acessar}>
+					<label htmlFor="email">E-mail*</label>
+					<input type="email" id="email" placeholder="Seu e-mail" onChange={event => setEmail(event.target.value)} value={email}/>
+
+					<button className="btn" type="submit">Acessar</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
+//parei no 41:50
